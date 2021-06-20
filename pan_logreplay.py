b924221dfac7251116359b27726ddbff806426ad
Michael B <michael.bell19282@gmail.com>
2021-06-20 17:39:17 +1000

    Initial commit

---
+++ pan_logreplay.py
@@ -0,0 +1,139 @@
+from os import write
+import panclient
+import csv, argparse
+from requests.utils import quote
+import xml.etree.ElementTree as ET
+
+def get_matchable_rows(csv_logfile: str):
+    #Get rid of the BoM marker by using utf-8-sig
+    reader = csv.DictReader(open(csv_logfile, mode='r', encoding='utf-8-sig'))
+    #Current supported test options in PANOS 10.1. If they arent present e.g. < 10.1 they will be ignored.
+    #Convert CSV columns to API parameters
+    csv_to_api_mappings = {
+        "Application" : "application",
+        "Category" : "category",
+        "Destination" : "destination",
+        "Destination address" : "destination",
+        "Destination Device Model" : "destination-model",
+        "Destination Category" : "destination-category",
+        "Destination OS" : "destination-os",
+        "Destination OS Family" : "destination-osfamily",
+        "Destination Profile" : "destination-profile",
+        "Destination Vendor" : "destination-vendor",
+        "Destination Port" : "destination-port",
+        "Source Zone" : "from",
+        "IP Protocol" : "protocol",
+        "Destination Zone" : "to",
+        "Source address" : "source",
+        "Source Device Category" : "source-category",
+        "Source Device Model" : "source-model",
+        "Source Device Profile" : "source-profile",
+        "Source Device Vendor" : "source-vendor",
+        "Source Device OS" : "source-os",
+        "Source Device OS Family" : "source-os",
+        "Source User" : "source-user"
+    }
+
+    mapped_rows = []
+    for row in reader:
+        new_normalised_row = {}
+        #Loop through the columns, translate them and grab the ones we are interested in
+        for key, value in row.items():
+            if key in row and key in csv_to_api_mappings:
+                new_key_name = csv_to_api_mappings[key]
+                #We need to convert TCP/UDP/ICMP to 6/17/1 (IP Protocol numbers)
+                if new_key_name == "protocol":
+                    value = translate_ip_protocol(value)
+                new_normalised_row[new_key_name] = value
+        #Save the old rule name for later comparison
+        new_normalised_row["Old Rule"] = row["Rule"]
+        if new_normalised_row:
+            mapped_rows.append(new_normalised_row)
+    
+    return mapped_rows
+
+
+def translate_ip_protocol(protocol: str):
+    #Translate ip protocol from tcp/udp/icmp to numerical value e.g. TCP -> 6, UDP -> 17
+    if protocol == "tcp":
+        return 6
+    if protocol == "udp":
+        return 17
+    if protocol == "icmp":
+        return 1
+    else:
+        #All other protocols *should* be represented as their numerical value already
+        #TODO: Check IPSEC/ESP protocols
+        return int(protocol)
+
+def get_rule_match(normalised_row: dict, client: panclient.PanClient):
+    ignored_keys = ["Old Rule"]
+    element_string = "<test><security-policy-match>"
+    for key, value in normalised_row.items():
+        #Ignore our 'Old Rule' field and any future others
+        if key in ignored_keys:
+            continue
+
+        #Remove destination port if we arent TCP or UDP
+        if normalised_row["protocol"] not in [6,17] and key == "destination-port":
+            continue
+
+        #Skip fields which are optional and empty
+        if value == "":
+            continue
+        
+        element_string += "<" + key + ">" + str(value) + "</" + key + ">"
+    
+    element_string += "</security-policy-match></test>"
+
+    result = client.get_xml_response("op", {"cmd" : quote(element_string)})
+    return result
+
+def create_comparison_csv(input_csv_filename: str, output_csv_filename: str, client: panclient.PanClient, output_progress: bool=True):
+    rows = get_matchable_rows(input_csv_filename)
+    if len(rows) == 0:
+        raise Exception("No valid rows found in " + input_csv_filename)
+
+    #Python 3.3+ outputs a nested list like ([1,2,3]), workaround to flatten
+    column_names = list(rows[0])
+    column_names.append("New Rule")
+    column_names.append("Equal")
+
+    writer = csv.DictWriter(open(output_csv_filename, mode="w", newline=""), fieldnames=column_names)
+    writer.writeheader()
+
+    position = 1
+    count = len(rows)
+
+    for row in rows:
+        xml_output = get_rule_match(row, client)
+
+        root_node = ET.fromstring(xml_output)
+        new_rule_name = root_node.find("./result/rules/entry").attrib["name"]
+
+        new_row = row
+        new_row["New Rule"] = new_rule_name
+        new_row["Equal"] = (new_row["Old Rule"] == new_rule_name)
+        writer.writerow(new_row)
+        if output_progress:
+            rules_are_equal = (row["Old Rule"] == new_rule_name)
+            print("[" + str(position) + "/" + str(count) + "] Old Rule: " + row["Old Rule"] + " == " + new_rule_name + ": " + str(rules_are_equal))
+            position += 1
+    
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_log_csv", help="PANOS CSV Log Export file")
+    parser.add_argument("output_log_csv", help="Comparison CSV to output")
+
+    parser.add_argument("hostname", help="PANOS firewall to test against")
+    parser.add_argument("apikey")
+
+    args = parser.parse_args()
+
+    client = panclient.PanClient(args.hostname, args.apikey)
+    create_comparison_csv(args.input_log_csv, args.output_log_csv, client)
+
+main()
---
+++ panclient.py
@@ -0,0 +1,225 @@
+import requests, os, time
+import xml.etree.ElementTree as ET
+from requests.utils import quote
+from enum import Enum
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class PanClient:
+
+    params = {
+        'hostname' : ''
+    }
+
+    options = {
+        'verify_certs' : False,
+        'https_port' : 443
+    }
+
+    device_type = Enum('Panorama', 'Standalone FW')
+
+    api_key = ''
+
+    system_info = {}
+
+    error_dict = {
+    '400' : 'Bad request',
+    '403' : 'Forbidden',
+    '1' : 'Unknown command',
+    '2' : 'Internal error',
+    '3' : 'Internal error',
+    '4' : 'Internal error',
+    '5' : 'Internal error',
+    '6' : 'Bad Xpath',
+    '7' : 'Object not present',
+    '8' : 'Object not unique',
+    '10' : 'Reference count not zero',
+    '11' : 'Internal error',
+    '13' : 'Internal error. (Possible config lock)',
+    '12' : 'Invalid object',
+    '14' : 'Operation not possible',
+    '15' : 'Operation denied',
+    '16' : 'Unauthorized',
+    '17' : 'Invalid command',
+    '18' : 'Malformed command',
+    '19' : 'Success',
+    '20' : 'Success',
+    '21' : 'Internal error',
+    '22' : 'Session timed out'}
+
+    def log_error(self, error, level):
+        print("[" + level + "]: " + error)
+
+    def construct_api_url(self, cmd_type, cmd):
+        hostname = self.params['hostname'] 
+        key = self.api_key
+        port = self.options['https_port']
+        
+        url = F"https://{hostname}:{port}/api/?key={key}&type={cmd_type}&{cmd}"
+        return url
+        
+    def get_xml(self, url):
+        response = requests.get(url, verify=self.options['validate_certs'])
+        return response.text
+    
+    def get_xml_response(self, cmd_type, dict_params):
+        hostname = self.params['hostname']
+        port = self.options['https_port']
+        url = F"https://{hostname}:{port}/api/?type={cmd_type}"
+        for k,v in dict_params.items():
+            url += "&" + k + "=" + v
+        
+        url += "&key=" + self.api_key
+
+        response = requests.get(url, verify=self.options['validate_certs'])
+        if self.options["debug"] == True:
+            print(response.request.url)
+        if self.options["debug"] == True:
+            print(response.text)
+        return response.text
+
+        #Converts command e.g show system info to <show><system><info></info></system></show>
+    def convert_cmd_to_xml(self, cmd, data=None):
+        input_items = cmd if isinstance(cmd, list) else cmd.split(" ")
+
+        final_output = ""
+        for item in input_items:
+            final_output += "<" + quote(item) + ">"
+        
+        if data is not None:
+            final_output += quote(str(data))
+
+        for item in reversed(input_items):
+            final_output += "</" + quote(item) + ">"
+
+        return final_output
+
+        
+
+
+    def get_api_key(self, username, password):
+        hostname = self.params['hostname']
+        port = self.options['https_port']
+        url = F"https://{hostname}:{port}/api/?type=keygen&user={username}&password={password}"
+
+        response = requests.get(url, verify=self.options['validate_certs'])
+        root = ET.fromstring(response.text)
+        key = root.find('result/key')
+
+        if key is not None:
+            return key.text
+        else:
+            self.log_error("Invalid credentials / API key not found", "ERROR")
+            return None
+
+
+    def check_errors(self, root_node):
+        if isinstance(root_node, str):
+            root_node = ET.fromstring(root_node)
+        if root_node.attrib['status'] == 'success':
+            if 'code' not in root_node.attrib:
+                return True
+            if root_node.attrib['code'] in ('19', '20'):
+                return True
+            elif root_node.attrib['code'] in ('7'):
+                self.log_error("Response returned success, but object not present", "WARN")
+                return True
+            else:
+                self.log_error("Response returned success, but non standard response code: " + root_node.attrib['code'], "WARN")
+                return True
+        elif 'code' in root_node.attrib:
+            self.log_error("Response returned error code: " + root_node.attrib['code'] + "\n" + str(root_node.text), "ERROR")
+            return False
+        elif 'code' not in root_node.attrib:
+            self.log_error("Response returned error", "ERROR")
+            return False
+
+    def wait_for_job(self, job_id):
+        cmd = "<show><jobs><id>" + job_id + "</id></jobs></show>"
+        rootNode = ET.fromstring(self.get_xml_response("op", {"cmd" : cmd }))
+
+        if self.check_errors(rootNode):
+            while(rootNode.find("./result/job/status").text != "FIN"):
+                time.sleep(1)
+                rootNode = ET.fromstring(self.get_xml_response("op", {"cmd" : cmd }))
+            return rootNode
+        else:
+            print("[ERROR] Waiting for job " + job_id + " failed with response " + ET.tostring(rootNode))
+
+    def commit(self, force=False, wait_for_completion=True, partial_admin_commit=""):
+        cmd = "<commit>"
+        if partial_admin_commit != "":
+            cmd += "<partial><admin><member>" + partial_admin_commit + "</member></admin></partial>"
+        if force:
+            cmd += "<force></force>"
+        cmd += "</commit>"
+
+        response = self.get_xml_response("commit", {"cmd" : cmd })
+        rootNode = ET.fromstring(response)
+        if self.check_errors(rootNode):
+            job_id = rootNode.find("./result/job").text
+            if wait_for_completion:
+                self.wait_for_job(job_id)
+            return job_id
+        else:
+            print("[ERROR] Queuing commit failed with error '" + ET.tostring(rootNode) + "'")
+
+    def push_device_groups(self, device_group, include_template=True, merge_with_candidate_config=True, validate_only=False, wait_for_job=True):
+        cmd = '<commit-all><shared-policy><device-group><entry name="' + device_group + '"></entry></device-group>'
+        if include_template:
+            cmd += "<include-template>yes</include-template>"
+        if merge_with_candidate_config:
+            cmd += "<merge-with-candidate-cfg>yes</merge-with-candidate-cfg>"
+        if validate_only:
+            cmd += "<validate-only>yes</validate-only>"
+        #This should not be done by automation! (in most cases) So we set to no
+        cmd += "<force-template-values>no</force-template-values>"
+        
+        cmd += "</shared-policy></commit-all>"
+
+        response = self.get_xml_response("commit", {"action" : "all", "cmd" : cmd })
+        rootNode = ET.fromstring(response)
+
+        if self.check_errors(rootNode):
+            job_id = rootNode.find("./result/job").text
+            if wait_for_job:
+                self.wait_for_job(job_id)
+            return job_id
+        else:
+            print("[ERROR] Queuing commit failed with error '" + ET.tostring(rootNode) + "'")
+
+
+
+            
+
+
+
+    def __init__(self, hostname, apikey, verify_certs=False, https_port=443,debug=False):
+        self.params['hostname'] = hostname
+        self.options['https_port'] = https_port
+        self.options['validate_certs'] = verify_certs
+
+        self.options["debug"] = debug
+
+        self.api_key = apikey
+        
+
+    #Method to generate class from credentials. Not preferred, use __init__/apikey where possible
+    @staticmethod
+    def from_credentials(hostname: str,  username: str, password: str, verify_certs: bool=False, https_port: int=443):
+        username = quote(username)
+        password = quote(password)
+        url = F"https://{hostname}:{https_port}/api/?type=keygen&user={username}&password={password}"
+
+        response = requests.get(url, verify=verify_certs)
+        root = ET.fromstring(response.text)
+        key = root.find('result/key')
+
+        if key is not None:
+            return PanClient(hostname, key, verify_certs=verify_certs, https_port=https_port)
+        else:
+            print("Invalid credentials / API key not found", "ERROR")
+            return None
+
+        
